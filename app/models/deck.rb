5cc971cef2b58b7d1a0a558429b99de04397386d
Dan <dchen12812@gmail.com>
2015-10-31 01:10:47 -0700

    added max character for deck title

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,5 +1,5 @@
 class Deck < ActiveRecord::Base
   belongs_to :user
-  validates :title, presence: true, uniqueness: {scope: :user_id}
+  validates :title, presence: true, uniqueness: {scope: :user_id}, length: {maximum: 30}
   validates :user_id, presence: true
 end
