9524525076a0a73ef56a62fab48a833d9c22f32b
Dan <dchen12812@gmail.com>
2015-11-02 14:57:06 -0800

    arrows based on next and previous entries

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,13 @@
 class Card < ActiveRecord::Base
 	belongs_to :deck
 	validates :front, :back, presence: true
+
+	#Checks if cards has more entry/deck
+	def next
+	  self.class.where("deck_id == ?", deck_id).where("id > ?", id).first
+	end
+
+	def previous
+	  self.class.where("deck_id == ?", deck_id).where("id < ?", id).last
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151102040301) do
+ActiveRecord::Schema.define(version: 20151102223920) do
 
   create_table "cards", force: :cascade do |t|
     t.text     "front"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20151102040301) do
     t.integer  "deck_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "number"
   end
 
   create_table "decks", force: :cascade do |t|
