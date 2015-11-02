dd1061038eeb655db0fe3ff3ae7cd3aa662ba5b5
Dan <dchen12812@gmail.com>
2015-11-01 18:31:58 -0800

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#Create users
+User.create(email: 					'example@test.com',
+			password: 			    'testtest',
+			password_confirmation: 'testtest'
+			)
+
+
+#Create decks
+(1..3).each do |i|
+Deck.create(title: "Example Deck #{i}",
+		 	user_id: 1
+	     	)
+end
