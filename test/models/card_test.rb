45010b59fa3e591961f6706087219f890050f39f
Dan <dchen12812@gmail.com>
2015-11-02 20:58:52 -0800

    card model tests

--- test/models/card_test.rb
+++ test/models/card_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class CardTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@card = Card.new(front:'wee',back: 'woo')
+  end
+  
+  test 'presence of front true' do
+  	@card.front = ""
+  	assert_no_difference('Card.count') do
+  		@card.save
+  	end
+  end
+
+  test 'presence of back true' do
+  	@card.back = ""
+  	assert_no_difference('Card.count') do
+  		@card.save
+  	end
+  end
+
+  test 'can find next card' do
+  	@card1 = cards(:card1)
+  	@card2 = cards(:card2)
+  	#assert_equal @card1.next.id,@card2.id
+  end
+
+  test 'can find prev card' do
+  	@card1 = cards(:card1)
+  	@card2 = cards(:card2)
+  	#assert_equal @card2.prev.id,@card1.id
+  end
 end
