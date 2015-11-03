29648bb64d30db10177511ea93fcac552e1759fe
Dan <dchen12812@gmail.com>
2015-11-02 20:15:14 -0800

    added some tests for decks controller

--- test/controllers/decks_controller_test.rb
+++ test/controllers/decks_controller_test.rb
@@ -2,36 +2,62 @@ require 'test_helper'
 
 class DecksControllerTest < ActionController::TestCase
   def setup
-  	@deck = decks(:deck1)
-  	@user = users(:user1)
+  	@deck1 = decks(:deck1)
+  	@deck2 = decks(:deck2)
+  	@user1 = users(:user1)
+  	@user2 = users(:user2)
   end
 
   #Testing controller functionality
-  test "new" do
-    assert true
-  end
-
-  test "create" do
+  test "should post create" do
+  	sign_in @user1
   	assert true
   end
 
-  test "edit" do
+  test "should patch edit" do
+  	sign_in @user1
   	assert true
   end
 
-  test "show" do
-
+  test "should show" do
+  	sign_in @user1
   end
 
-  test "destroy" do
-
+  test "should delete destroy" do
+  	sign_in @user1
   end
 
-  test "redirect if not logged in" do
-  	
+  #Testing require login
+  test "should redirect from create if not logged in" do
+  	post(:create, deck:{title: 'cat'})
+  	assert_redirected_to root_path
   end
 
-  test "redirect if not correct user or shared" do
-
+  test "should redirect from edit if not logged in" do
+  	patch(:edit, {id: @deck1.id, deck:{title: 'cat'}})
+  	assert_redirected_to root_path
   end
+
+  test "should redirect from show if not logged in" do
+  	get(:show, {id:@deck1.id})
+  	assert_redirected_to root_path
+  end
+
+  test "should redirect from update if not logged in" do
+  	patch(:update, {id: @deck1.id, deck:{title: 'cat'}})
+  	assert_redirected_to root_path
+  end  
+
+  test "should redirect from destroy if not logged in" do
+  	delete(:destroy, {id:@deck1.id})
+  	assert_redirected_to root_path
+  end
+
+  #Testing correct_user
+  test "should redirect if not correct user or shared" do
+  	sign_in @user2
+  end
+
+  #Testing last_visited
 end
+
